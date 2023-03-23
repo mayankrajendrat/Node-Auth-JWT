4ffe84c96aa7298187c48188009ddd49f53c930e
Mayank Tiwari <mayank_tiwari@epam.com>
2023-03-23 19:13:09 +0530

    Added DB URL to ENV

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 module.exports ={
     'secret' : "10xauthsecret",
     //'db':"mongodb://localhost:27017/node-auth"
-    'db':"mongodb+srv://root:10xacademy@cluster0.mq96yxs.mongodb.net/?retryWrites=true&w=majority"
+    'db':process.env.DB_URL
 }
